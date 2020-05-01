362daf34907b934bae2ac22b91c674f27d95978c
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-05-01 15:43:26 +0200

    Update Electra Repo Link To Chimera Repo Link

--- installbootstrap.sh
+++ installbootstrap.sh
@@ -2,7 +2,7 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mWelcome to Chimera Bootstrap Install Utility V1.1.5 (Beta) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
+echo -e "\e[31mWelcome to Chimera Bootstrap Install Utility V1.1.5.1 (Beta) By @Yaya4_4 Follow Me On Twitter Pls.\e[0m"
 echo "Installing Dependency For The Installer"
 apt update
 apt install unzip -y
@@ -24,41 +24,41 @@ cp /User/Documents/ChimeraBootstrap/launchctl /bin/launchctl
 chmod +x /bin/launchctl
 cp -r -f -u /User/Documents/ChimeraBootstrap/bootstrap/* /
 echo "Done. Downloading necessary debs."
-wget https://electrarepo64.coolstar.org/debs/cydia_2.3_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/zip_3.0_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/wget_1.1.9_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/zsh_5.7.1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/cydia_2.3_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/zip_3.0_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/wget_1.1.9_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/zsh_5.7.1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
 wget https://github.com/Yaya48/ChimeraBootstrapInstaller-Checkrain-/raw/master/debs/apt_1.8.2_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
 wget https://github.com/Yaya48/ChimeraBootstrapInstaller-Checkrain-/raw/master/debs/apt-key_1.8.2-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
 wget https://github.com/Yaya48/ChimeraBootstrapInstaller-Checkrain-/raw/master/debs/apt-lib_1.8.2-sileo6_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/bash_4.4.18-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/bsdprep_1.0_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/bzip2_1.0.6-2_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/coreutils_8.30-1_iphoneos_arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/coreutils-bin_8.30-3_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/darwintools_1-7_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/debianutils_4.8.4-2_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/diffutils_3.7_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/diskdev_cmds_593-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/dpkg_1.19.4-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/file_3.3_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/file-cmds_272.250.1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/findutils_4.7_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/gnupg_1.4.23_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/grep_3.4_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/lzma_5.2.4-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/ncurses_6.1-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/p7zip_16.02_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/profile.d_0-4_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/sed_4.8_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/shell_cmds_203-3_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/tar_1.31-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/text-cmds_99-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/uikittools_2.0.1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/unrar_5.5.8_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/unzip_6.0_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/org.coolstar.sileo_1.7.4_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
-wget https://electrarepo64.coolstar.org/debs/org.swift.libswift_5.0-electra2_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/bash_4.4.18-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/bsdprep_1.0_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/bzip2_1.0.6-2_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/coreutils_8.30-1_iphoneos_arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/coreutils-bin_8.30-3_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/darwintools_1-7_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/debianutils_4.8.4-2_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/diffutils_3.7_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/diskdev_cmds_593-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/dpkg_1.19.4-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/file_3.3_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/file-cmds_272.250.1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/findutils_4.7_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/gnupg_1.4.23_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/grep_3.4_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/lzma_5.2.4-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/ncurses_6.1-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/p7zip_16.02_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/profile.d_0-4_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/sed_4.8_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/shell_cmds_203-3_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/tar_1.31-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/text-cmds_99-1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/uikittools_2.0.1_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/unrar_5.5.8_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/unzip_6.0_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/org.coolstar.sileo_1.7.4_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
+wget https://repo.chimera.sh/debs/org.swift.libswift_5.0-electra2_iphoneos-arm.deb --directory-prefix=/User/Documents/ChimeraBootstrap
 echo "Done. Installing necessary debs."
 dpkg -i --force-all /User/Documents/ChimeraBootstrap/zip_3.0_iphoneos-arm.deb
 dpkg -i --force-all /User/Documents/ChimeraBootstrap/wget_1.1.9_iphoneos-arm.deb
