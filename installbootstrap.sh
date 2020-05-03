c2c56ac1641a3c66a33dcc572d8ba6faff73e379
Yaya4 <34249303+Yaya48@users.noreply.github.com>
2020-05-03 02:58:26 +0200

    Update installbootstrap.sh

--- installbootstrap.sh
+++ installbootstrap.sh
@@ -2,5 +2,5 @@
 if [ "$EUID" -ne 0 ]; then
 echo Please run this script as root.
 else
-echo -e "\e[31mHe you try to use ChimeraBootsrapInstaller By @Yaya4_4 But This Script Is EOL and deprecated pls use the official script from coolstar tuto on DISCORD:https://discordapp.com/invite/YRD4mSA \e[0m"
+echo -e "\e[31mHi you try to use ChimeraBootsrapInstaller By @Yaya4_4 But This Script Is EOL and deprecated pls use the official script from coolstar tuto on DISCORD:https://discordapp.com/invite/YRD4mSA \e[0m"
 fi
